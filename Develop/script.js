eabd7d19d7839ec72cb9459129fc0e2028d48442
Mellllow <131720670+Mellllow@users.noreply.github.com>
2023-05-18 18:28:36 -0700

    Add files via upload

--- Develop/script.js
+++ Develop/script.js
@@ -16,40 +16,59 @@
 
 
 
-// Assignment code here
-// have user click button and generate a random code
-  //button click > PROMPTS TO check/uncheck criteria, upper, lower, numbers, specials, and LENGTH (8-128)
-//use an array full of all numbers, vartters, and 1-8 shift mode to pull from
-//click generate to keep looking 
-//promts for lower/upper case, numeric, and specials
-
-//for loop for
-  //alert prompt boxes
-  //
-
-//pull from here
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specials = ["!", "@", "#", "$", "%","^", "&", "*",];
-
-alert("choose your criterias");
-window.prompt("Pick your Criterias\n lower\n upper\n numbers\n specials", "Variables here")
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = 8;
-  const all = lower+upper+numbers+specials;
-  return all[ Math.floor(random(all.length))];
-
+  passwordText.value = password
 }
 
+function generatePassword(){
+    var passlength = prompt("how many characters would you like? (8-128)");
+    while((passlength < 8) || ( passlength > 128) || (isNaN(passlength) === true)){
+        alert("Not a valid imput. Please input a number value between 8-128")
+        passlength = prompt("how many characters would you like? (8-128)");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-//function getPassword(length,lower, upper, specials, numbers)
+    }
+    //ask questions for characters 
+    var confirmLowerCase = confirm("Would you like to add Lowercase Characters?");
+    var confirmUppperCase = confirm("Would you like to add UpperCase Characters?");
+    var confirmNumbers = confirm("Would you like to add Numbers?");
+    var confirmSpecials = confirm("Would you like to add Special Characters?");
+    
+    //create var to hold characters
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var specials = "!@#$%^&*"
+    
+    var characterBank = "";
+    var newPassword = "";
+    //add if statment depednging on user answer
+    if (confirmLowerCase === true){
+        characterBank = characterBank.concat(lowerCase)
+    }
+    if (confirmUppperCase === true){
+        characterBank = characterBank.concat(upperCase)
+    }
+    if (confirmNumbers === true){
+        characterBank = characterBank.concat(numbers)
+    }
+    if (confirmSpecials === true){
+        characterBank = characterBank.concat(specials)
+    }
+    //grabs all and mixups the variables and produces password here
+    function mixupBank(max){
+        return [Math.floor(Math.random() * max)]
+    }
+    // equations for password generation
+    for (var i = 0; i < passlength; i++){
+        newPassword = newPassword.concat(characterBank.charAt(mixupBank(characterBank.length - 1)))
+    }
+    return newPassword
+}
